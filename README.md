586a9dbe234b56cbe06f4a57f91c0248ce552690
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2023-05-05 22:27:56 +0200

    setup repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gradle Unicode Example
+
+This repository simply contains a minimal [Gradle](https://gradle.org) project that showcases Gradle internal
+Unicode/UTF-8 support (or lack thereof).
+
+For this example this is specifically limited to parsing and handling `gradle.properties`.
+
+It relates to the following issues:
+
+- [Gradle#19235](https://github.com/gradle/gradle/issues/19235)
+- [Gradle#16763](https://github.com/gradle/gradle/issues/16763)
---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+}
+
+group = "com.nikolasgrottendieck"
+version = "1.0-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	testImplementation(platform("org.junit:junit-bom:5.9.1"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.compileJava {
+	options.encoding = "UTF-8"
+}
+
+tasks.compileTestJava {
+	options.encoding = "UTF-8"
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
+tasks.processResources {
+	expand(project.properties)
+}
